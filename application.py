901941f1b57509d53cee60e35a9381fbc6ee4e0d
Nitish Singh <nitman118@gmail.com>
2019-02-04 16:04:33 +0100

    added new code, html files, a milestone really...

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template,request,flash,redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,55 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
-def index():
-    return "Project 1: TODO"
+@app.route("/", methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        session.clear()
+        return render_template("login.html")
+    elif request.method=="POST":
+        uname = request.form.get("username")
+        pin=request.form.get("password")
+        row=db.execute("SELECT id,username,password FROM users WHERE username = :username ",{"username":uname}).fetchone()
+        if row is None:
+            return "No entry found"
+        elif pin!=row.password or uname=="":
+            return "Invalid Credentials"
+        else:
+            session["u_id"]=row.id
+            return render_template("search.html")
+
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method=="GET":
+        return render_template("register.html")
+    elif request.method=="POST":
+        u=request.form.get("username")
+        p=request.form.get("password")
+        p2=request.form.get("password2")
+        text=request.form.get("submit")
+        if p!=p2 or len(p)==len(p2)==0 or len(u)==0:
+            return redirect(url_for('error'))
+        else:
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": u, "password": p})
+            db.commit()
+            flash('Your account was created successfully!')
+            return redirect("/")
+
+@app.route("/search", methods=["GET","POST"])
+def search():
+    if request.method=="GET":
+        return render_template("search.html")
+
+    elif request.method=="POST":
+        radioOpt = request.form.get("inlineRadioOptions")
+        return radioOpt
+
+
+
+
+
+
+@app.route("/error")
+def error():
+    return "Error"
