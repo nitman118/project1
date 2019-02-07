be1eb468653f6c535aaf423c8cab6d9ad4833a26
Nitish Singh <nitman118@gmail.com>
2019-02-07 01:38:34 +0100

    added new css

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 import os
 import requests
 
-from flask import Flask, session, render_template,request,flash,redirect, url_for,abort
+from flask import Flask, session, render_template, request, flash, redirect, url_for, abort
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -12,6 +13,8 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
+
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -19,11 +22,12 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-goodreadsDevKey="9k8LxnWmW5tXr6hd7iHQ"
+goodreadsDevKey = "9k8LxnWmW5tXr6hd7iHQ"
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -35,87 +39,102 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def login():
-    if request.method=="GET":
+    if request.method == "GET":
         session.clear()
         return render_template("login.html")
-    elif request.method=="POST":
+    elif request.method == "POST":
         uname = request.form.get("username")
-        pin=request.form.get("password")
-        row=db.execute("SELECT id,username,password FROM users WHERE username = :username ",{"username":uname}).fetchone()
+        pin = request.form.get("password")
+        row = db.execute("SELECT id,username,password FROM users WHERE username = :username ", {
+                         "username": uname}).fetchone()
         if row is None:
             return "No entry found"
-        elif pin!=row.password or uname=="":
+        elif pin != row.password or uname == "":
             return "Invalid Credentials"
         else:
-            session["u_id"]=row.id
+            session["u_id"] = row.id
             return render_template("search.html")
 
 
-@app.route("/register", methods=["GET","POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method=="GET":
+    if request.method == "GET":
         return render_template("register.html")
-    elif request.method=="POST":
-        u=request.form.get("username")
-        p=request.form.get("password")
-        p2=request.form.get("password2")
-        text=request.form.get("submit")
-        if p!=p2 or len(p)==len(p2)==0 or len(u)==0:
+    elif request.method == "POST":
+        u = request.form.get("username")
+        p = request.form.get("password")
+        p2 = request.form.get("password2")
+        text = request.form.get("submit")
+        if p != p2 or len(p) == len(p2) == 0 or len(u) == 0:
             return redirect(url_for('error'))
         else:
-            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": u, "password": p})
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {
+                       "username": u, "password": p})
             db.commit()
             flash('Your account was created successfully!')
             return redirect("/")
 
 
-@app.route("/search", methods=["GET","POST"])
+@app.route("/search", methods=["GET", "POST"])
+@login_required
 def search():
-    if request.method=="GET":
+    if request.method == "GET":
         return render_template("search.html")
-    elif request.method=="POST":
+    elif request.method == "POST":
         keyword = request.form.get("keyword")
-        keyword="%"+keyword+"%"
-        keyword=keyword.upper()
+        keyword = "%"+keyword+"%"
+        keyword = keyword.upper()
         radioOpt = request.form.get("inlineRadioOptions")
-        if keyword.strip("%")=="":
+        if keyword.strip("%") == "":
             return "Empty KeyWord"
-        if radioOpt=="option1":
-            searchResults = db.execute("SELECT * FROM books WHERE UPPER(title) LIKE :title ORDER BY year DESC",{"title":keyword}).fetchall()
-        elif radioOpt=="option2":
-            searchResults = db.execute("SELECT * FROM books WHERE UPPER(author) LIKE :author ORDER BY year DESC",{"author":keyword}).fetchall()
-        elif radioOpt=="option3":
-            searchResults = db.execute("SELECT * FROM books WHERE year = :year ORDER BY author ASC",{"year":int(keyword.strip('%'))}).fetchall()
-        elif radioOpt=="option4":
-            searchResults = db.execute("SELECT * FROM books WHERE isbn LIKE :isbn ORDER BY year DESC",{"isbn":keyword}).fetchall()
+        if radioOpt == "option1":
+            searchResults = db.execute(
+                "SELECT * FROM books WHERE UPPER(title) LIKE :title ORDER BY year DESC", {"title": keyword}).fetchall()
+        elif radioOpt == "option2":
+            searchResults = db.execute(
+                "SELECT * FROM books WHERE UPPER(author) LIKE :author ORDER BY year DESC", {"author": keyword}).fetchall()
+        elif radioOpt == "option3":
+            searchResults = db.execute("SELECT * FROM books WHERE year = :year ORDER BY author ASC", {
+                                       "year": int(keyword.strip('%'))}).fetchall()
+        elif radioOpt == "option4":
+            searchResults = db.execute(
+                "SELECT * FROM books WHERE isbn LIKE :isbn ORDER BY year DESC", {"isbn": keyword}).fetchall()
         return render_template("searchResult.html", searchResults=searchResults)
 
 
-@app.route("/search_result/<isbn>", methods=["GET","POST"])
+@app.route("/search_result/<isbn>", methods=["GET", "POST"])
+@login_required
 def bookDetails(isbn):
-    if request.method=="GET":
+    if request.method == "GET":
         #create a connection
         print("get received")
-        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": goodreadsDevKey, "isbns": isbn})
+        res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                           params={"key": goodreadsDevKey, "isbns": isbn})
         if res is None:
             return redirect(url_for('error'))
-        json_data=res.json()["books"][0]
-        book=db.execute("SELECT * FROM books WHERE isbn = :isbn ORDER BY year DESC",{"isbn":isbn}).fetchone()
-        revs=db.execute("SELECT * FROM reviews WHERE isbn=:isbn",{"isbn":isbn}).fetchall()
-        return render_template("book.html",book=book, json_data=json_data,revs=revs)
-    elif request.method=="POST":
+        json_data = res.json()["books"][0]
+        book = db.execute(
+            "SELECT * FROM books WHERE isbn = :isbn ORDER BY year DESC", {"isbn": isbn}).fetchone()
+        revs = db.execute(
+            "SELECT * FROM reviews WHERE isbn=:isbn", {"isbn": isbn}).fetchall()
+        return render_template("book.html", book=book, json_data=json_data, revs=revs)
+    elif request.method == "POST":
         print("post received")
         review = request.form.get("review")
         rating = request.form.get("inlineRadioOptions")
-        user=db.execute("SELECT * FROM users WHERE id=:id",{"id":session["u_id"]}).fetchone()
-        review_count=db.execute("SELECT COUNT(*) FROM reviews WHERE reviewer = :reviewer AND isbn=:isbn",{"reviewer":user.username,"isbn":isbn}).fetchall()
-        if len(review)!=0 and int(review_count[0][0])==0:
-            book=db.execute("SELECT * FROM books WHERE isbn = :isbn ORDER BY year DESC",{"isbn":isbn}).fetchone()
-            db.execute("INSERT INTO reviews (isbn,reviewer,review,rating) VALUES (:isbn,:reviewer,:review,:rating)",{"isbn":isbn,"reviewer":user.username,"review":review,"rating":rating})
+        user = db.execute("SELECT * FROM users WHERE id=:id",
+                          {"id": session["u_id"]}).fetchone()
+        review_count = db.execute("SELECT COUNT(*) FROM reviews WHERE reviewer = :reviewer AND isbn=:isbn", {
+                                  "reviewer": user.username, "isbn": isbn}).fetchall()
+        if len(review) != 0 and int(review_count[0][0]) == 0:
+            book = db.execute(
+                "SELECT * FROM books WHERE isbn = :isbn ORDER BY year DESC", {"isbn": isbn}).fetchone()
+            db.execute("INSERT INTO reviews (isbn,reviewer,review,rating) VALUES (:isbn,:reviewer,:review,:rating)", {
+                       "isbn": isbn, "reviewer": user.username, "review": review, "rating": rating})
             db.commit()
-            return redirect(url_for("bookDetails",isbn=isbn))
+            return redirect(url_for("bookDetails", isbn=isbn))
         else:
             return "You have already posted"
 
---
+++ helpers.py
@@ -0,0 +1,16 @@
+from flask import redirect, render_template, request, session, url_for
+from functools import wraps
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("u_id") is None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
